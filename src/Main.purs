f900211a9690da814d40761abd5fd19752458bb7
Daniel Schraudner <daniel.schraudner@fau.de>
2022-10-06 16:49:24 +0200

    Can POST windows

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,11 @@ module Main where
 
 import Prelude
 
-import Data.Array (concat, dropEnd, length, mapWithIndex, range, (!!), (:))
+import Data.Array (concat, dropEnd, filter, head, length, mapWithIndex, range, snoc, (!!))
 import Data.Array as Array
+import Data.Either (hush)
 import Data.Formatter.Interval (formatInterval)
+import Data.Formatter.Parser.Interval (parseIsoDuration)
 import Data.Int (fromString)
 import Data.Interval (Interval(..))
 import Data.Interval.Duration.Iso (IsoDuration)
@@ -15,11 +17,12 @@ import Data.String.CaseInsensitive (CaseInsensitiveString(..))
 import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Effect.Ref (Ref, modify_, new, read)
-import HTTPure (Method(..), Request, ResponseM, ServerM, created, header, notFound, ok', serve, toString, (!@))
+import HTTPure (Method(..), Request, ResponseM, ServerM, badRequest, created, header, notFound, ok', serve, toString, (!@))
 import HTTPure.Headers (Headers(..))
 import N3 (Format(..), parse, write)
 import Node.Process (argv)
-import RDF (Graph, Quad, Term, blankNode, defaultGraph, literalType, namedNode, namedNode', quad)
+import Parsing (runParser)
+import RDF (Graph, Quad, Term, blankNode, defaultGraph, literalType, namedNode, namedNode', object, predicate, quad, value)
 import RDF.Prefixes (Prefix(..), ldp, rdf, xsd)
 
 ldpsc :: Prefix
@@ -33,10 +36,13 @@ emptyStreamContainer :: StreamContainer
 emptyStreamContainer = StreamContainer [] []
 
 addGraphToStreamContainer :: Graph -> StreamContainer -> StreamContainer
-addGraphToStreamContainer graph (StreamContainer graphArray windowArray) = StreamContainer (graph : graphArray) windowArray
+addGraphToStreamContainer graph (StreamContainer graphArray windowArray) = StreamContainer (snoc graphArray graph) windowArray
 
 getGraphFromStreamContainer :: Int -> StreamContainer -> Maybe Graph
 getGraphFromStreamContainer i (StreamContainer graphArray _) = graphArray !! i
+
+addWindowToStreamContainer :: Window -> StreamContainer -> StreamContainer
+addWindowToStreamContainer window (StreamContainer graphArray windowArray) = StreamContainer graphArray (snoc windowArray $ window)
 
 nextGraphId :: StreamContainer -> Int
 nextGraphId (StreamContainer graphArray _) = length graphArray
@@ -58,6 +64,15 @@ windowToQuads i (Window memberRelation membershipResource contentTimestampRelati
 ]
   where
     window = blankNode $ "window-" <> show i
+
+quadsToWindow :: Array Quad -> Maybe Window
+quadsToWindow quads = do
+  memberRelation <- head $ filter (\quad -> predicate quad == namedNode' ldp "hasMemberRelation") quads
+  membershipResource <- head $ filter (\quad -> predicate quad == namedNode' ldp "hasMembershipResource") quads
+  contentTimestampRelation <- head $ filter (\quad -> predicate quad == namedNode' ldpsc "hasContentTimestampRelation") quads
+  logical <- head $ filter (\quad -> predicate quad == namedNode' ldpsc "logical") quads :: Maybe Quad
+  duration <- hush $ runParser (value $ object logical) parseIsoDuration
+  pure $ Window (object memberRelation) (object membershipResource) (object contentTimestampRelation) duration
 
 formatForMIME :: Maybe String -> Format
 formatForMIME (Just "text/turtle") = Turtle
@@ -88,11 +103,20 @@ router port streamContainerRef { method: Get, path, headers: (Headers headers) }
     Just graph -> do
       payload <- write ("http://localhost:" <> show port <> "/" <> show i) (formatForMIME $ lookup (CaseInsensitiveString "Accept") headers) $ Array.fromFoldable graph
       ok' (header "Content-Type" "text/turtle") payload
+-- POST /window
+router port streamContainerRef { method: Post, path: ["window"], body, headers: (Headers headers) } = do
+  bodyString <- toString body
+  payload <- parse ("http://localhost:" <> show port <> "/") (formatForMIME $ lookup (CaseInsensitiveString "Content-Type") headers) bodyString
+  case addWindowToStreamContainer <$> quadsToWindow payload of 
+    Nothing -> badRequest "Could not parse window"
+    Just window -> do
+      liftEffect $ modify_ window streamContainerRef
+      created
 router _ _ _ = notFound
 
 main :: ServerM
 main = do
-  streamContainerRef <- new emptyStreamContainer
+  streamContainerRef <- new $ emptyStreamContainer
   args <- argv
   let port = fromMaybe 8080 do
         portString <- args !! 2

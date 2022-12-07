54130b9af845aa6d5d4d14a4db007802d81f6e80
Daniel Schraudner <daniel.schraudner@fau.de>
2022-12-07 18:16:35 +0100

    Cleaned up code

--- src/Main.purs
+++ src/Main.purs
@@ -9,7 +9,7 @@ import Affjax.ResponseFormat (string)
 import CLI (Builtin(..), Options, Stream(..), Terms(..), optsInfo)
 import Control.Alternative (guard)
 import Control.Parallel (parSequence)
-import Data.Array (catMaybes, concatMap, filter, findIndex, foldl, index, last, length, mapMaybe, mapWithIndex, (!!))
+import Data.Array (catMaybes, concatMap, filter, findIndex, foldl, index, length, mapMaybe, mapWithIndex, (!!))
 import Data.Array as Array
 import Data.Array.NonEmpty (foldl1)
 import Data.Array.NonEmpty as NonEmpty
@@ -25,7 +25,7 @@ import Data.Maybe (Maybe(..), fromMaybe)
 import Data.Number (fromString)
 import Data.Set (Set, member)
 import Data.Set as Set
-import Data.String (Pattern(..), contains, joinWith, split)
+import Data.String (joinWith)
 import Data.Time.Duration (negateDuration)
 import Data.Time.Duration as Duration
 import Data.Tuple (Tuple(..), snd)
@@ -37,107 +37,8 @@ import Effect.Now (nowDateTime)
 import Effect.Timer (setInterval)
 import N3 (Format(..), parse, write)
 import Options.Applicative (execParser)
-import RDF (Quad, Term, defaultGraph, literalType, namedNode, namedNode', object, predicate, quad, subject, termType, value, variable)
-import RDF.Prefixes (Prefix(..), rdf, xsd)
-
---data Triple = Triple TermOrVariable TermOrVariable TermOrVariable
---instance showTriple :: Show Triple where
---  show (Triple p s o) = show p <> "(" <> show s <> "," <> show o <> ")"
-
---data Aggregation = MMax | MMin | MSum | MCount | MAvg
---instance showAggregation :: Show Aggregation where
---  show MMax = "mmax"
---  show MMin = "mmin"
---  show MSum = "msum"
---  show MCount = "mcount"
---  show MAvg = "mavg"
-
---data Rule = Diamond Triple Triple | Box Triple Triple | And Triple (Array Triple)-- | Agg Aggregation Triple TermOrVariable Triple
---instance showRule :: Show Rule where
---  show (Diamond head body) = show head <> " ⟵ " <> "◇" <> show body
---  show (Box head body) = show head <> " ⟵ " <> "□" <> show body
---  show (And head triples) = show head <> " ⟵ " <> (joinWith " ∧ " $ map show triples)
---  show (Agg aggregation head result body) = show head <> " ⟵ " <> show result <> " = " <> show body
-
---rule :: Rule
-----rule = Box (Triple (NoVariable $ namedNode "http://example.org/blinkedRightLast3Step") (Variable "car") (Variable "value")) (Triple (NoVariable $ namedNode "http://example.org/properties/blinkerRight") (Variable "car") (Variable "value"))
---rule = Agg MCount (
---    Triple
---    (NoVariable $ namedNode "blinkedRightCount")
---    (Variable "CAR")
---    (Variable "M")
---  ) (Variable "M") (
---    Triple 
---    (NoVariable $ namedNode "http://example.org/properties/blinkerRight")
---    (Variable "CAR")
---    (NoVariable $ literalType "true" $ namedNode' xsd "boolean")
---  )
-
---data TermOrVariable = NoVariable Term | Variable String
---derive instance eqTermOrVariable :: Eq TermOrVariable
---derive instance ordTermOrVariable :: Ord TermOrVariable
---instance showTermOrVariable :: Show TermOrVariable where
---  show (NoVariable term) = fromMaybe (show term) $ getSuffix term
---  show (Variable variable) = toUpper variable
---
---data Fact = Fact Term Term Term DateTime
---derive instance eqFact :: Eq Fact
---derive instance ordFact :: Ord Fact
---instance showFact :: Show Fact where
---  show (Fact predicate subject object time) = p <> "(" <> s <> "," <> o <> ")@" <> t <> "."
---    where
---      p = (fromMaybe (show predicate) $ getSuffix predicate)
---      s = (fromMaybe (show subject) $ getSuffix subject)
---      o = (fromMaybe (show object) $ getSuffix object)
---      t = format iso8601Formatter time
-
---type Binding = Map TermOrVariable Term
---
---getBinding :: Fact -> Triple -> Maybe Binding
---getBinding (Fact p s o _) (Triple p' s' o') = do
---  pBind <- bindTerm p p'
---  sBind <- bindTerm s s'
---  oBind <- bindTerm o o'
---  pure $ union pBind $ union sBind oBind
---
---applyBinding :: DateTime -> Binding -> Triple -> Maybe Fact
---applyBinding now binding (Triple p s o) = do
---  p' <- replaceVariable binding p
---  s' <- replaceVariable binding s
---  o' <- replaceVariable binding o
---  pure $ Fact p' s' o' now
---
---replaceVariable :: Binding -> TermOrVariable -> Maybe Term
---replaceVariable _ (NoVariable term) = Just term
---replaceVariable binding var = lookup var binding
---
---bindTerm :: Term -> TermOrVariable -> Maybe Binding
---bindTerm term (NoVariable term') = if term == term' then Just empty else Nothing
---bindTerm term var = Just $ singleton var term
---
---applyRule :: DateTime -> Rule -> Array Fact -> Array Fact
---applyRule now (Diamond head body) facts = nub $ catMaybes $ map (\b -> applyBinding now b head) bindings
---  where
---    bindings = catMaybes $ map (\fact -> getBinding fact body) facts
---applyRule now (Box head body) facts = if length bindings == length facts && length (nub bindings) == 1 then nub $ catMaybes $ map (\b -> applyBinding now b head) bindings else []
---  where
---    bindings = catMaybes $ map (\fact -> getBinding fact body) facts
---applyRule now (And head triples) facts = if length bindings == length facts then nub $ catMaybes [ applyBinding now (foldl union empty bindings) head ] else []
---  where
---    bindings = catMaybes $ concat $ map (\fact -> map (\triple -> getBinding fact triple) triples) facts
-----applyRule now (Agg MCount head result body) facts = trace body \_ -> trace facts \_ -> nub $ catMaybes $ map (\b -> applyBinding now b head) $ map (insert result value) bindings
-----  where
-----    bindings = catMaybes $ map (\fact -> getBinding fact body) facts
-----    value = literalType (show $ length bindings) $ namedNode' xsd "integer"
---applyRule _ _ _ = []
-
-sosa :: Prefix
-sosa = Prefix "http://www.w3.org/ns/sosa/"
-
-getSuffix :: Term -> Maybe String
-getSuffix namedNode | termType namedNode == "NamedNode" = if contains (Pattern "#") (value namedNode) then last (split (Pattern "#") $ value namedNode) else  last (split (Pattern "/") $ value namedNode)
-getSuffix literal | termType literal == "Literal" = Just $ value literal
-getSuffix _ = Nothing
+import RDF (Quad, Term, defaultGraph, literalType, namedNode, namedNode', object, predicate, quad, subject, termType, value)
+import RDF.Prefixes (rdf, xsd)
 
 iso8601Formatter :: Formatter
 iso8601Formatter = fromFoldable
@@ -154,32 +55,6 @@ iso8601Formatter = fromFoldable
   , SecondsTwoDigits
   , Placeholder "Z"
   ]
-
-membershipResource :: Term
-membershipResource = namedNode "http://localhost:8082/#window"
-
-memberRelation :: Term
-memberRelation = namedNode "http://localhost:8082/#inWindow"
-
-main :: Effect Unit
-main = do
-  opts <-execParser optsInfo
-  logShow opts
-  _ <- setInterval 1000 $ loop opts
-  pure unit
-
-loop :: Options -> Effect Unit
-loop opts = launchAff_ do
-  dateTime <- liftEffect $ nowDateTime
-  -- TODO Remove + 1
-  let roundedDateTime = fromMaybe dateTime $ adjust (negateDuration $ (\ms -> Duration.Milliseconds ms) $ toNumber $ (\i -> i + 1) $ fromEnum $ millisecond $ time dateTime) dateTime
-  liftEffect $ log $ "Time: " <> format iso8601Formatter roundedDateTime
-  relations <- parSequence $ map (getRelationForSource roundedDateTime) (Array.fromFoldable opts.sources) :: Aff (Array Relation)
-  let joined = fromMaybe (newRelation (RelationHeader [])) $ (foldl1 naturalJoin) <$> (NonEmpty.fromArray relations)
-  let builtinJoined = foldl filterBuiltin joined $ Array.fromFoldable opts.builtins
-  postRelationToGoal roundedDateTime opts.goal builtinJoined
-  liftEffect $ logShow builtinJoined
-  pure unit
 
 filterBuiltin :: Relation -> Builtin -> Relation
 filterBuiltin (Relation (RelationHeader header) rows) (LessThanEqual term1 term2) = Relation (RelationHeader header) $ mapMaybe filterRow rows
@@ -218,12 +93,6 @@ newRelation header = Relation header []
 
 addRow :: RelationRow -> Relation -> Relation
 addRow newRow (Relation header rows) = Relation header $ rows <> [ newRow ]
-
-r1 :: Relation
-r1 = addRow (RelationRow [ namedNode "http://ex.org/cars/3", literalType "10" (namedNode' xsd "integer") ]) $ addRow (RelationRow [ namedNode "http://ex.org/cars/1", literalType "3" (namedNode' xsd "integer") ]) $ newRelation $ RelationHeader [ variable "car", variable "speed" ]
-
-r2 :: Relation
-r2 = addRow (RelationRow [ namedNode "http://ex.org/colors/green", namedNode "http://ex.org/cars/2" ]) $ addRow (RelationRow [ namedNode "http://ex.org/colors/red", namedNode "http://ex.org/cars/3" ]) $ newRelation $ RelationHeader [ variable "color", variable "car" ]
 
 naturalJoin :: Relation -> Relation -> Relation
 naturalJoin (Relation (RelationHeader header1) rows1) (Relation (RelationHeader header2) rows2) = Relation combinedHeaders $ catMaybes $ joinRow <$> rows1 <*> rows2
@@ -276,24 +145,6 @@ postRelationToGoal dateTime (Stream uri pred variables) (Relation (RelationHeade
         i <- findIndex (\h -> h == head) header
         row !! i
 
-
---datalogFromQuads :: Array Quad -> Maybe Fact
---datalogFromQuads quads = do
---  p <- object <$> find (\q -> predicate q == namedNode' sosa "observedProperty") quads
---  s <- object <$> find (\q -> predicate q == namedNode' sosa "hasFeatureOfInterest") quads
---  o <- object <$> find (\q -> predicate q == namedNode' sosa "hasSimpleResult") quads
---  t <- join $ (\dateTimeString -> hush $ runParser dateTimeString parseDateTime) <$> value <$> object <$> find (\q -> predicate q == namedNode' sosa "resultTime") quads
---  pure $ Fact p s o t
---
---quadsFromDatalog :: Fact -> Array Quad
---quadsFromDatalog (Fact p s o t) = [
---  quad (namedNode "") (namedNode' rdf "type") (namedNode' sosa "Observation") defaultGraph,
---  quad (namedNode "") (namedNode' sosa "resultTime") (literalType (format iso8601Formatter t) (namedNode' xsd "dateTimeStamp")) defaultGraph,
---  quad (namedNode "") (namedNode' sosa "hasSimpleResult") o defaultGraph,
---  quad (namedNode "") (namedNode' sosa "hasFeatureOfInterest") s defaultGraph,
---  quad (namedNode "") (namedNode' sosa "observedProperty") p defaultGraph
---]
-
 getQuads :: DateTime -> URL -> Aff (Array Quad)
 getQuads acceptDateTime url = do
   liftEffect $ log ("GET " <> url)
@@ -314,3 +165,21 @@ postQuads quads url = do
       liftEffect $ log $ printError error
       pure unit
     Right _ -> pure unit
+
+main :: Effect Unit
+main = do
+  opts <-execParser optsInfo
+  logShow opts
+  void $ setInterval 1000 $ loop opts
+
+loop :: Options -> Effect Unit
+loop opts = launchAff_ do
+  dateTime <- liftEffect $ nowDateTime
+  -- TODO Remove + 1
+  let roundedDateTime = fromMaybe dateTime $ adjust (negateDuration $ (\ms -> Duration.Milliseconds ms) $ toNumber $ (\i -> i + 1) $ fromEnum $ millisecond $ time dateTime) dateTime
+  liftEffect $ log $ "Time: " <> format iso8601Formatter roundedDateTime
+  relations <- parSequence $ map (getRelationForSource roundedDateTime) (Array.fromFoldable opts.sources) :: Aff (Array Relation)
+  let joined = fromMaybe (newRelation (RelationHeader [])) $ (foldl1 naturalJoin) <$> (NonEmpty.fromArray relations)
+  let builtinJoined = foldl filterBuiltin joined $ Array.fromFoldable opts.builtins
+  postRelationToGoal roundedDateTime opts.goal builtinJoined
+  liftEffect $ logShow builtinJoined

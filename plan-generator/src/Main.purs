a645ec4bdf1b4f7bf1852700446e4a10caaa3fd3
Daniel Schraudner <daniel.schraudner@fau.de>
2023-05-11 11:02:55 +0200

    MainNew to Main

--- plan-generator/src/Main.purs
+++ plan-generator/src/Main.purs
@@ -1,104 +1,96 @@
-module Main where
+module Main
+  ( Plan(..)
+  , ReasoningNode(..)
+  , ReasoningType(..)
+  , StreamNode(..)
+  , Window(..)
+  , createPlan
+  , getTermsWithWindows
+  , getTermsWithWindowsRule
+  )
+  where
 
 import Prelude
 
---import Data.Array (mapWithIndex)
---import Data.Array as Array
---import Data.Map (toUnfoldable)
---import Data.Map as Map
---import Data.Set (Set)
---import Data.Set as Set
---import Data.String (joinWith)
---import Data.Tuple (Tuple(..))
---import DatalogMTL (Formula(..), Interval(..), Predicate(..), Program, Rule(..), Term(..), getIntervallsForPredicates, normalForm)
---import Effect (Effect)
---import Effect.Console (log)
---import Node.Encoding (Encoding(..))
---import Node.FS.Sync (writeTextFile)
---
---data Window = Window Int Int StreamContainer
---
---data StreamContainer = StreamContainer Term
---
---data Agent = And Window (Array Window) | Box Window Window | Diamond Window Window
---
---data LogicalPlan = LogicalPlan (Array Agent)
---
-----getLogicalPlan :: Program -> LogicalPlan
-----getLogicalPlan program = LogicalPlan []
-----  where
-----    streamContainers :: Map String StreamContainer
-----    streamContainers = foldl (\m r -> ) Map.empty program
-----      where
-----        
---
---dockerCompose :: Program -> String
---dockerCompose program = "services:\n"
---  <> (joinWith "\n" $ mapWithIndex (\i (Tuple predicate intervals) -> "  "
---    <> show predicate
---    <> "-stream-container:\n"
---    <> "    command: node index.js -p "
---    <> show (9000 + i)
---    <> joinWith "" (map (\(Interval start end) -> " -w \"#window-" <> show start <> "-" <> show end <> " http://ex.org/inWindow http://ex.org/timestamp http://ex.org/poisened http://ex.org/isPoisonous "  <> show start <> " " <> show end <> "\"") (Set.toUnfoldable intervals :: Array Interval))
---    <> "\n    ports:\n      - \""
---    <> show (9000 + i)
---    <> ":"
---    <> show (9000 + i)
---    <> "\"\n    image: stream-container:latest") streamContainerList)
---    where
---      streamContainerList :: Array (Tuple Predicate (Set Interval))
---      streamContainerList = Map.toUnfoldable $ getIntervallsForPredicates $ normalForm program
---
---dot :: Program -> String
---dot program = "digraph G {\n" <> joinWith "\n" (map scNode (toUnfoldable $ getIntervallsForPredicates program)) <> "\n" <> joinWith "\n" (map agentNode program) <> "\n}"
---  where
---  scNode :: Tuple Predicate (Set Interval) -> String
---  scNode (Tuple (Predicate predicate) intervals) = "  " <> predicate <> " [shape=record, label=\"{" <> predicate <> (if Set.size intervals > 0 then ("|{" <> joinWith "|" (map (\(Interval start end) -> "<w_" <> show start <> "_" <> show end <> ">" <> "[" <> show start <> "," <> show end <> "]") (Array.fromFoldable intervals)) <> "}") else "") <> "}\"];"
---  agentNode :: Rule -> String
---  agentNode (Rule (Pred (Predicate headPred) _) [ BoxPlus (Interval start end) (Pred (Predicate (bodyPred)) _) ]) = "  " <> agentName <> " [shape=circle, label=\"□\"];\n  " <> bodyPred <> ":<w_" <> show start <> "_" <> show end <> ">" <> " -> " <> agentName <> ";\n  " <> agentName <> " -> " <> headPred <> ";"
---    where
---      agentName = "agent_" <> bodyPred <> "_BoxPlus_" <> headPred
---  agentNode (Rule (Pred (Predicate headPred) _) [ BoxMinus (Interval start end) (Pred (Predicate (bodyPred)) _) ]) = "  " <> agentName <> " [shape=circle, label=\"□\"];\n  " <> bodyPred <> ":<w_" <> show (negate start) <> "_" <> show (negate end) <> ">" <> " -> " <> agentName <> ";\n  " <> agentName <> " -> " <> headPred <> ";"
---    where
---      agentName = "agent_" <> bodyPred <> "_BoxMinus_" <> headPred
---  agentNode (Rule (Pred (Predicate headPred) _) [ DiamondPlus (Interval start end) (Pred (Predicate (bodyPred)) _) ]) = "  " <> agentName <> " [shape=circle, label=\"◇\"];\n  " <> bodyPred <> ":<w_" <> show start <> "_" <> show end <> ">" <> " -> " <> agentName <> ";\n  " <> agentName <> " -> " <> headPred <> ";"
---    where
---      agentName = "agent_" <> bodyPred <> "_DiamondPlus_" <> headPred
---  agentNode (Rule (Pred (Predicate headPred) _) [ DiamondMinus (Interval start end) (Pred (Predicate (bodyPred)) _) ]) = "  " <> agentName <> " [shape=circle, label=\"◇\"];\n  " <> bodyPred <> ":<w_" <> show (negate start) <> "_" <> show (negate end) <> ">" <> " -> " <> agentName <> ";\n  " <> agentName <> " -> " <> headPred <> ";"
---    where
---      agentName = "agent_" <> bodyPred <> "_DiamondMinus_" <> headPred
---  agentNode (Rule (Pred (Predicate headPred) _) body) = "  " <> agentName <> " [shape=circle, label=\"∧\"];\n" <> joinWith "\n" (map (\f -> "  " <> getPredicateString f <> ":<w_0_0> -> " <> agentName <> ";") body) <> "  " <> agentName <> " -> " <> headPred <> ";"
---    where
---      getPredicateString :: Formula -> String
---      getPredicateString (Pred (Predicate predicate) _) = predicate
---      getPredicateString _ = "// Something went wrong here (Formula too deep)!"
---      agentName = "agent_" <> joinWith "_" (map getPredicateString body) <> "_BoxPlus_" <> headPred
---  agentNode (Rule _ _ ) = "// Something went wrong here (No Match)!"
---
---showProgram :: Program -> String
---showProgram rules = "[\n" <> joinWith "\n" (map (\r -> "  " <> show r) rules) <> "\n]"
---
---speedLessThanEqual30 :: Rule
---speedLessThanEqual30 = Rule (Pred (Predicate "speed_less_than_equal_30") [ Variable "car" ]) [ Pred (Predicate "speed") [ Variable "car", Variable "speed" ], Pred (Predicate "less_than_equal") [ Variable "speed", Constant "30" ] ]
---
---speed0 :: Rule
---speed0 = Rule (Pred (Predicate "speed_less_than_equal_0") [ Variable "car" ]) [ Pred (Predicate "speed") [ Variable "car", Variable "speed" ], Pred (Predicate "less_than_equal") [ Variable "speed", Constant "0" ] ]
---
---lightjam :: Rule
---lightjam = Rule (Pred (Predicate "light_jam") [ Variable "car" ]) [ BoxMinus (Interval 0 15) (Pred (Predicate "speed_less_than_equal_30") [ Variable "car" ]) ]
---
---mediumjam :: Rule
---mediumjam = Rule (Pred (Predicate "medium_jam") [ Variable "car" ]) [ Pred (Predicate "light_jam") [ Variable "car" ], DiamondMinus (Interval 0 30) (BoxMinus (Interval 0 3) (Pred (Predicate "speed_less_than_equal_0") [ Variable "car" ])) ]
---
---heavyjam :: Rule
---heavyjam = Rule (Pred (Predicate "heavy_jam") [ Variable "car" ]) [ Pred (Predicate "light_jam") [ Variable "car" ], BoxMinus (Interval 0 30) (DiamondMinus (Interval 0 10) (BoxMinus (Interval 0 3) (Pred (Predicate "speed_less_than_equal_0") [ Variable "car" ]))) ]
---
---jam :: Program
---jam = [ speedLessThanEqual30, speed0, lightjam, mediumjam, heavyjam ]
---
---main :: Effect Unit
---main = do
---  --log $ showProgram jam
---  log $ showProgram $ normalForm jam
---  writeTextFile UTF8 "plan.dot" (dot $ normalForm jam)
---  writeTextFile UTF8 "docker-compose.yml" (dockerCompose $ normalForm jam)
+import Data.Array (catMaybes, concat, filter, fromFoldable, groupAllBy)
+import Data.Array as Array
+import Data.Array.NonEmpty (head, toArray)
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Set (Set)
+import Data.Set as Set
+import Data.Tuple (Tuple(..), fst, snd)
+import DatalogMTL (Formula(..), Interval(..), Predicate(..), Program, Rule(..), Term)
+
+data StreamNode = StreamNode Predicate (Array Term)
+
+instance eqStreamNode :: Eq StreamNode where
+  eq (StreamNode (Predicate p1) _) (StreamNode (Predicate p2) _) = p1 == p2
+
+instance ordStreamNode :: Ord StreamNode where
+  compare (StreamNode (Predicate p1) _) (StreamNode (Predicate p2) _) = compare p1 p2
+
+instance showStreamNode :: Show StreamNode where
+  show (StreamNode pred _) = show pred
+
+data Window = Window StreamNode Int Int 
+
+instance showWindow :: Show Window where
+  show (Window streamNode start end) = show streamNode <> "[" <> show start <> ", " <> show end <> "]"
+
+derive instance eqWindow :: Eq Window
+
+derive instance ordWindow :: Ord Window
+
+data ReasoningNode = ReasoningNode ReasoningType (Array Window) StreamNode
+
+instance showReasoning :: Show ReasoningNode where
+  show (ReasoningNode reasoningType windows streamNode) = show reasoningType <> ": " <> show windows <> " -> " <> show streamNode
+
+data ReasoningType = Box | Diamond | And | Agg
+
+instance showReasoningType :: Show ReasoningType where
+  show Box = "□"
+  show Diamond = "◇"
+  show And = "∧"
+  show Agg = "A"
+
+data Plan = Plan (Array ReasoningNode) (Array StreamNode) (Map StreamNode (Set Window))
+
+instance showPlan :: Show Plan where
+  show (Plan rNodes sNodes wToS) = show rNodes <> "\n" <> show sNodes <> "\n" <> show wToS
+
+createPlan :: Program -> Plan
+createPlan program = Plan (catMaybes $ map ruleToReasoningNode program) (fromFoldable $ fst <$> Map.values streamNodesMap) (Map.fromFoldable $ map (\(Tuple sn ws) -> Tuple sn (Set.fromFoldable $ ws)) $ Map.values streamNodesMap)
+  where
+    ruleToReasoningNode :: Rule -> Maybe ReasoningNode
+    ruleToReasoningNode (Rule (Pred predH termsH) [ BoxPlus (Interval start end) (Pred predB _) ]) = Just $ ReasoningNode Box [ (getWindow predB start end) ] $ StreamNode predH termsH
+    ruleToReasoningNode (Rule (Pred predH termsH) [ BoxMinus (Interval start end) (Pred predB _) ]) = Just $ ReasoningNode Box [ (getWindow predB (negate start) (negate end)) ] $ StreamNode predH termsH
+    ruleToReasoningNode (Rule (Pred predH termsH) [ DiamondPlus (Interval start end) (Pred predB _) ]) = Just $ ReasoningNode Diamond [ (getWindow predB start end) ] $ StreamNode predH termsH
+    ruleToReasoningNode (Rule (Pred predH termsH) [ DiamondMinus (Interval start end) (Pred predB _) ]) = Just $ ReasoningNode Diamond [ (getWindow predB (negate start) (negate end)) ] $ StreamNode predH termsH
+    ruleToReasoningNode (Rule (Pred predH termsH) conj) = Just $ ReasoningNode And (catMaybes $ map getWindowConj conj) $ StreamNode predH termsH
+    ruleToReasoningNode (AggrRule (Pred predH termsH) _ _ (Pred predB _)) = Just $ ReasoningNode Agg [ getWindow predB 0 0 ] $ StreamNode predH termsH
+    ruleToReasoningNode _ = Nothing
+    getWindowConj :: Formula -> Maybe Window
+    getWindowConj (Pred p _) = Just $ getWindow p 0 0
+    getWindowConj _ = Nothing
+    getWindow :: Predicate -> Int -> Int -> Window
+    getWindow predicate start end = fromMaybe (Window (StreamNode (Predicate "error") []) 0 0) $ Array.head $ filter (\(Window _ s e) -> start == s && end == e) $ fromMaybe [] $ snd <$> Map.lookup predicate streamNodesMap
+    streamNodesMap :: Map Predicate (Tuple StreamNode (Array Window))
+    streamNodesMap = Map.fromFoldableWith (\(Tuple sNode1 windows1) (Tuple _ windows2) -> (Tuple sNode1 $ windows1 <> windows2)) $ map (\group -> Tuple (fst $ fst $ head group) (Tuple (StreamNode (fst $ fst $ head group) (snd $ fst $ head group)) (map ((\(Tuple _ (Tuple start end)) -> Window (StreamNode (fst $ fst $ head group) (snd $ fst $ head group)) start (negate end))) $ toArray group))) $ groupAllBy (\(Tuple p1 _) (Tuple p2 _) -> compare p1 p2) $ getTermsWithWindows program
+
+getTermsWithWindows :: Program -> Array (Tuple (Tuple Predicate (Array Term)) (Tuple Int Int))
+getTermsWithWindows program = concat $ map getTermsWithWindowsRule program
+
+getTermsWithWindowsRule :: Rule -> Array (Tuple (Tuple Predicate (Array Term)) (Tuple Int Int))
+getTermsWithWindowsRule (Rule head body) = getTermsWithWindowsFormula head <> (concat $ map getTermsWithWindowsFormula body)
+getTermsWithWindowsRule (AggrRule head _ _ body) = getTermsWithWindowsFormula head <> getTermsWithWindowsFormula body
+
+getTermsWithWindowsFormula :: Formula -> Array (Tuple (Tuple Predicate (Array Term)) (Tuple Int Int))
+getTermsWithWindowsFormula (Pred predicate terms) = [ Tuple (Tuple predicate terms) (Tuple 0 0) ]
+getTermsWithWindowsFormula (BoxPlus (Interval start end) (Pred predicate terms)) = [ Tuple (Tuple predicate terms) (Tuple start end) ]
+getTermsWithWindowsFormula (BoxMinus (Interval start end) (Pred predicate terms)) = [ Tuple (Tuple predicate terms) (Tuple (negate start) (negate end)) ]
+getTermsWithWindowsFormula (DiamondPlus (Interval start end) (Pred predicate terms)) = [ Tuple (Tuple predicate terms) (Tuple start end) ]
+getTermsWithWindowsFormula (DiamondMinus (Interval start end) (Pred predicate terms)) = [ Tuple (Tuple predicate terms) (Tuple (negate start) (negate end)) ]
+getTermsWithWindowsFormula _ = []

7dbf88d0e7495cf5ff318e23cc3f27bad030bded
Daniel Schraudner <daniel.schraudner@fau.de>
2023-05-11 11:39:27 +0200

    DockerCompose: Specify Inputs

--- plan-generator/spago.dhall
+++ plan-generator/spago.dhall
@@ -24,6 +24,7 @@ to generate this file without the comments in this block.
   , "node-fs"
   , "ordered-collections"
   , "prelude"
+  , "rdf"
   , "strings"
   , "tuples"
   ]
--- plan-generator/src/Backends/DockerCompose.purs
+++ plan-generator/src/Backends/DockerCompose.purs
@@ -3,7 +3,7 @@ module Backends.DockerCompose where
 import Prelude
 
 import Backends.Dot (runningExample)
-import Data.Array (concat, fold, fromFoldable, mapWithIndex)
+import Data.Array (concat, filter, fold, fromFoldable, mapWithIndex)
 import Data.Map (Map)
 import Data.Map as Map
 import Data.Maybe (fromMaybe)
@@ -11,13 +11,21 @@ import Data.Set (Set)
 import Data.Set as Set
 import Data.String (joinWith)
 import Data.Tuple (Tuple(..))
-import DatalogMTL (Program, normalForm)
+import DatalogMTL (Predicate(..), Program, normalForm)
 import Dodo (Doc, break, foldWithSeparator, indent, plainText, print, text, twoSpaces)
 import Effect (Effect)
 import Effect.Console (log, logShow)
-import MainNew (Plan(..), ReasoningNode(..), ReasoningType(..), StreamNode(..), Window(..), createPlan)
+import Main (Plan(..), ReasoningNode(..), ReasoningType(..), StreamNode(..), Window(..), createPlan)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync (writeTextFile)
+import RDF (Term, literalType, namedNode, namedNode', value)
+import RDF.Prefixes (xsd)
+
+data Parameters = Parameters {
+  inputs :: Array Input
+}
+
+data Input = Input Term Predicate (Array Term)
 
 data Compose = Compose (Array Service) (Array Network) (Array Volume) (Array Config) (Array Secret)
 
@@ -36,17 +44,23 @@ data Config = Config
 
 data Secret = Secret
 
-planToCompose :: Plan -> Compose
-planToCompose (Plan rNodes sNodes wToS) = Compose (mapWithIndex (streamNodeToService wToS) sNodes <> mapWithIndex reasoningNodeToService rNodes) [] [] [] []
+planToCompose :: Parameters -> Plan -> Compose
+planToCompose parameters (Plan rNodes sNodes wToS) = Compose (mapWithIndex (streamNodeToService parameters wToS) sNodes <> mapWithIndex reasoningNodeToService rNodes) [] [] [] []
 
-streamNodeToService :: Map StreamNode (Set Window) -> Int -> StreamNode -> Service
-streamNodeToService wToS i sNode@(StreamNode pred _) = Service (show pred) { image: StreamContainer, ports: Map.singleton (9000 + i) 8080, command: [
+streamNodeToService :: Parameters -> Map StreamNode (Set Window) -> Int -> StreamNode -> Service
+streamNodeToService (Parameters parameters) wToS i sNode@(StreamNode pred _) = Service (show pred) { image: StreamContainer, ports: Map.singleton (9000 + i) 8080, command: [
   "node",
-  "index.js"
+  "index.js",
+  "-p",
+  "http://vocab.ex.org/" <> show pred
 ] <> (concat (map (\(Window _ start end) -> [
   "-w",
   "#window" <> show start <> "_" <> show end <> " " <> show start <> " " <> show end 
-]) $ fromFoldable $ fromMaybe Set.empty $ Map.lookup sNode wToS)) }
+]) $ fromFoldable $ fromMaybe Set.empty $ Map.lookup sNode wToS)) <> (concat (map (\(Input subject _ objects) -> [
+  "-d",
+  value subject <> " [" <> joinWith "," (map value objects) <> "]"
+]) $ filter (\(Input _ predicate _) -> predicate == pred) parameters.inputs))
+}
 
 reasoningNodeToService :: Int -> ReasoningNode -> Service
 reasoningNodeToService i (ReasoningNode reasoningType windows (StreamNode pred terms)) = Service ("rn" <> show i) { image: ReasoningAgent, ports: Map.empty, command: [
@@ -83,5 +97,18 @@ main = do
   log ""
   logShow $ createPlan $ normalForm runningExample
   log ""
-  log $ print plainText twoSpaces $ composeToDoc $ planToCompose $ createPlan $ normalForm runningExample
-  writeTextFile UTF8 "compose.yaml" (print plainText twoSpaces $ composeToDoc $ planToCompose $ createPlan $ normalForm runningExample)
+  let parameters = Parameters { inputs: [
+    Input (namedNode "http://cars.ex.org/car1") (Predicate "speed") [
+      literalType "10" (namedNode' xsd "integer"),
+      literalType "12" (namedNode' xsd "integer"),
+      literalType "14" (namedNode' xsd "integer"),
+      literalType "16" (namedNode' xsd "integer"),
+      literalType "18" (namedNode' xsd "integer"),
+      literalType "20" (namedNode' xsd "integer"),
+      literalType "20" (namedNode' xsd "integer"),
+      literalType "20" (namedNode' xsd "integer"),
+      literalType "15" (namedNode' xsd "integer")
+    ]
+  ] }
+  log $ print plainText twoSpaces $ composeToDoc $ planToCompose parameters $ createPlan $ normalForm runningExample
+  writeTextFile UTF8 "compose.yaml" (print plainText twoSpaces $ composeToDoc $ planToCompose parameters $ createPlan $ normalForm runningExample)
